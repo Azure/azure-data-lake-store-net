eb521b40e905eff57fb8cd7ff097f331e6555d3e
Piotr Wolszakiewicz (POWO) <powo@demant-technology.com>
2019-07-09 14:17:36 +0200

    Add .net standard 2.0 + remove user agent header from non net requests

--- AdlsDotNetSDK/WebTransport.cs
+++ AdlsDotNetSDK/WebTransport.cs
@@ -323,12 +323,10 @@ namespace Microsoft.Azure.DataLake.Store
                     webReq.Headers[key] = customHeaders[key];
                 }
             }
-#if NET452
+#if NETSTANDARD2_0 || NET452
             webReq.UserAgent = client.GetUserAgent();
             webReq.ServicePoint.UseNagleAlgorithm = false;
             webReq.ServicePoint.Expect100Continue = false;
-#else
-            webReq.Headers["User-Agent"] = client.GetUserAgent();
 #endif
             webReq.Headers["x-ms-client-request-id"] = req.RequestId;
             webReq.Method = opMethod;
